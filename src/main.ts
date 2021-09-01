a53217613fd6f5fdcc746be5c49e85829e5b6916
Aleksandr Yahno <aleksandryahno@gmail.com>
2021-09-01 15:48:49 +0300

    add project

---
+++ src/main.ts
@@ -0,0 +1,22 @@
+import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { AppModule } from './app.module';
+
+
+async function start() {
+    const PORT = process.env.PORT || 5000
+    const app = await NestFactory.create(AppModule)
+
+    const config = new DocumentBuilder()
+        .setTitle('Nest App')
+        .setDescription('Documentation REST API')
+        .setVersion('1.0.0')
+        .addTag('NEST')
+        .build()
+    const document = SwaggerModule.createDocument(app, config)
+    SwaggerModule.setup('/api/docs', app, document)
+
+    await  app.listen(PORT, () => console.log(`port ${PORT}`))
+}
+
+start()
---
+++ src/users/users.model.ts
@@ -0,0 +1,35 @@
+import { BelongsToMany, Column, DataType, Model, Table } from 'sequelize-typescript';
+import { ApiProperty } from '@nestjs/swagger';
+import { Role } from '../roles/roles.model';
+import { UserRoles } from '../roles/user-roles.model';
+
+interface UserCreationAttrs {
+    email: string;
+    password: string;
+}
+
+@Table({tableName: 'users'})
+export class User extends Model<User, UserCreationAttrs> {
+    @ApiProperty({example: '1', description: 'uniq id'})
+    @Column({type: DataType.INTEGER, unique: true, autoIncrement: true, primaryKey: true})
+    id: number;
+
+    @ApiProperty({example: 'user@gmail.com', description: 'email'})
+    @Column({type: DataType.STRING, unique: true, allowNull: false})
+    email: string;
+
+    @ApiProperty({example: '123password456', description: 'password'})
+    @Column({type: DataType.STRING, allowNull: false})
+    password: string;
+
+    @ApiProperty({example: 'true', description: 'banned or not'})
+    @Column({type: DataType.BOOLEAN, defaultValue: false})
+    banned: boolean;
+
+    @ApiProperty({example: 'spam', description: 'ban reason'})
+    @Column({type: DataType.BOOLEAN, allowNull: true})
+    banReason: string;
+
+    @BelongsToMany(() => Role, () => UserRoles)
+    roles: Role[];
+}
